0a6133701f8a4deee1b524fa0551cf60c5e6071e
Ranmocy <ranmocy@gmail.com>
2012-02-11 16:16:05 +1000

    remove el-get/

--- el-get/.status.el
+++
@@ -1,1 +0,0 @@
-(:el-get "installed" :package "installed" :ruby-mode "installed" :inf-ruby "installed" :textmate "installed" :rvm "installed" :rhtml "installed" :yaml-mode "installed" :css-mode "installed" :ruby-compilation "installed" :cucumber "installed" :twittering-mode "installed" :markdown-mode "installed")
