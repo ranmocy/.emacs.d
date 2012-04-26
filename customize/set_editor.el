4dbe8f98449d41621380ef535d74fbe6013a8cab
Ranmocy <ranmocy@gmail.com>
2012-04-26 14:58:27 +0800

    fix evil

--- customize/set_editor.el
+++ customize/set_editor.el
@@ -51,18 +51,24 @@
 ;;----------evil----------
 (eval-after-load "evil"
   '(progn
+     (defun evil-undefine ()
+       (interactive)
+       (let (evil-mode-map-alist)
+         (call-interactively (key-binding (this-command-keys)))))
+
      (define-key evil-normal-state-map "\C-r" 'isearch-backward)
      (define-key evil-normal-state-map "\C-e" 'end-of-line)
      (define-key evil-normal-state-map "\C-n" 'next-line)
      (define-key evil-normal-state-map "\C-p" 'previous-line)
 
-     (define-key evil-insert-state-map "\C-r" 'isearch-backward)
-     (define-key evil-insert-state-map "\C-e" 'end-of-line)
-     (define-key evil-insert-state-map "\C-n" 'next-line)
-     (define-key evil-insert-state-map "\C-p" 'previous-line)
+     (define-key evil-insert-state-map "\C-k" 'evil-undefine)
+     (define-key evil-insert-state-map "\C-r" 'evil-undefine)
+     (define-key evil-insert-state-map "\C-e" 'evil-undefine)
+     (define-key evil-insert-state-map "\C-n" 'evil-undefine)
+     (define-key evil-insert-state-map "\C-p" 'evil-undefine)
 
      ;; viper-cua-region-fix
-     (define-key evil-normal-state-map [backspace] 'backward-delete-char-untabify)
+     ;; (define-key evil-normal-state-map [backspace] 'backward-delete-char-untabify)
      (define-key evil-insert-state-map [backspace] 'backward-delete-char-untabify)
      ;; (define-key viper-vi-global-user-map "\C-d" 'delete-char)
      ;; (define-key viper-insert-global-user-map "\C-d" 'delete-char)
@@ -73,7 +79,7 @@
      ;; (setq evil-visual-state-cursor '("LightBlue" box))
      ;; (setq evil-motion-state-cursor '("LightBlue" box))
      ;; (setq evil-replace-state-cursor '("LightBlue" box))
-     ;; (setq evil-operator-state-cursor '("LightBlue" box))
+     ;; (ksetq evil-operator-state-cursor '("LightBlue" box))
      (setq evil-emacs-state-cursor '("Red" box))
      ))
 (add-to-list 'load-path "~/.emacs.d/plugins/evil")
