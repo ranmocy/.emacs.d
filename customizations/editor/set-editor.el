ca7f2cc8d23368f875a6d18b099e515f4f8d4c17
Ranmocy <ranmocy@gmail.com>
2012-05-20 11:29:09 +0800

    add paredit

--- customizations/editor/set-editor.el
+++ customizations/editor/set-editor.el
@@ -4,14 +4,15 @@
 (require 'set-ibus)
 (require 'set-ispell)
 (require 'set-search)
+(require 'set-paredit)
 
 (setq
- kill-ring-max 1024          ;用一个很大的kill ring这样防止不小心删掉重要的东西
+ kill-ring-max 1024           ;用一个很大的kill ring这样防止不小心删掉重要的东西
  undo-outer-limit 5000000     ;撤销限制
  mark-ring-max 1024           ;设置的mark ring容量
  global-mark-ring-max 1024    ;设置最大的全局标记容量
  ediff-window-setup-function (quote ediff-setup-windows-plain) ;设置窗口设置在同一个frame里
- kill-whole-line t
+ kill-whole-line t                      ;删除整行
  x-select-enable-clipboard t            ;支持emacs和外部程序的粘贴
  default-fill-column 80                 ;默认显示 80列就换行
  )
