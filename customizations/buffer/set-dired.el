23a5b08b36fa9cc30d4bc6c7363731588d29745e
Ranmocy <ranmocy@gmail.com>
2012-05-24 12:06:16 +0800

    fix diredful conf not loaded

--- customizations/buffer/set-dired.el
+++ customizations/buffer/set-dired.el
@@ -2,12 +2,12 @@
  '(:name diredful
          :type git
          :url "git://github.com/emacsmirror/diredful.git"
-         :after (lambda ()
-                  (setq diredful-init-file (expand-file-name "diredful-conf.el" (file-name-directory file)))))
+         :before (lambda ()
+                   (setq diredful-init-file (expand-file-name "diredful-conf.el" (file-name-directory (locate-library "set-dired"))))))
  '(:name dired-details
          :type git
          :url "git://github.com/emacsmirror/dired-details.git"
-         :after (lambda ()
+         :before (lambda ()
                   (setq dired-omit-files "^\\...+$")))
  '(:name dired-details-plus
          :type git
